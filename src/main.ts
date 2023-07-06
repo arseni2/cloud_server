3f9052232b5232cad1aa09d8416804931c52a967
arseni2 <arc-37@mail.ru>
2023-07-07 00:21:14 +0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,50 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import * as express from 'express';
-import { join } from 'path';
+// import {NestFactory} from '@nestjs/core';
+// import {AppModule} from './app.module';
+// import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+// import * as express from 'express';
+// import { join } from 'path';
+
+// async function bootstrap() {
+//     const app = await NestFactory.create(AppModule, {cors: false});
+
+//     app.enableCors({credentials: true, origin: "*"})
+
+//     app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+
+//     const config = new DocumentBuilder()
+//         .setTitle('API')
+//         .setDescription('The CloudStorage API description')
+//         .setVersion('1.0')
+//         .addBearerAuth()
+//         .build();
+//     const document = SwaggerModule.createDocument(app, config);
+//     SwaggerModule.setup('swagger', app, document);
+//     SwaggerModule.setup('swagger', app, document, {
+//         swaggerOptions: {
+//             persistAuthorization: true,
+//         },
+//     });
+
+//     await app.listen(8000);
+// }
+
+// bootstrap();
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import serverless = require('serverless-http');
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {cors: false});
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = '.netlify/functions/main';
+  app.setGlobalPrefix(globalPrefix);
+  await app.init();
 
-    app.enableCors({credentials: true, origin: "*"})
-
-    app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
-
-    const config = new DocumentBuilder()
-        .setTitle('API')
-        .setDescription('The CloudStorage API description')
-        .setVersion('1.0')
-        .addBearerAuth()
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('swagger', app, document);
-    SwaggerModule.setup('swagger', app, document, {
-        swaggerOptions: {
-            persistAuthorization: true,
-        },
-    });
-
-    await app.listen(8000);
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverless(expressApp)
 }
 
-bootstrap();
+let server;
+export const handler = async (event, context, callback) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
